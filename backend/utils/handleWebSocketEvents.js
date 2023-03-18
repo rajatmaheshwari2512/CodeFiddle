b1fb6655ec7478c4ed9a663373f454c873e4f3c6
Rajat Maheshwari <mrajat67@yahoo.com>
2023-03-18 17:24:04 +0530

    Added socket events for handling changes in files and folders

--- backend/utils/handleWebSocketEvents.js
+++ backend/utils/handleWebSocketEvents.js
@@ -1,8 +1,132 @@
 const fs = require("fs");
 const path = require("path");
 
-const handleWebSocketEvents = (ws, id, type, data, path) => {
-  // const pathToFile=
+const handleWebSocketEvents = (ws, id, type, data, pathToFileOrFolder) => {
+  const pathToFileOrFolder = path.resolve(
+    `${__dirname}/playgrounds/${id}/${pathToFileOrFolder}`
+  );
+  switch (type) {
+    case "writeFile":
+      fs.writeFile(pathToFileOrFolder, data, (err) => {
+        if (err) {
+          console.log(err);
+          const errMessage = {
+            type: "error",
+            payload: {
+              data: "Could not write to file",
+            },
+          };
+          ws.send(JSON.stringify(errMessage));
+        } else {
+          const successMessage = {
+            type: "success",
+            payload: {
+              data: "File written successfully",
+            },
+          };
+          ws.send(JSON.stringify(successMessage));
+        }
+      });
+      break;
+    case "readFile":
+      fs.readFile(pathToFileOrFolder, (err, data) => {
+        if (err) {
+          console.log(err);
+          const errMessage = {
+            type: "error",
+            payload: {
+              data: "Could not read file",
+            },
+          };
+          ws.send(JSON.stringify(errMessage));
+        } else {
+          const successMessage = {
+            type: "success",
+            payload: {
+              data: data.toString(),
+            },
+          };
+          ws.send(JSON.stringify(successMessage));
+        }
+      });
+      break;
+    case "deleteFile":
+      fs.unlink(pathToFileOrFolder, (err) => {
+        if (err) {
+          console.log(err);
+          const errMessage = {
+            type: "error",
+            payload: {
+              data: "Could not delete file",
+            },
+          };
+          ws.send(JSON.stringify(errMessage));
+        } else {
+          const successMessage = {
+            type: "success",
+            payload: {
+              data: "File deleted successfully",
+            },
+          };
+          ws.send(JSON.stringify(successMessage));
+        }
+      });
+      break;
+    case "createFolder":
+      fs.mkdir(pathToFileOrFolder, (err) => {
+        if (err) {
+          console.log(err);
+          const errMessage = {
+            type: "error",
+            payload: {
+              data: "Could not create folder",
+            },
+          };
+          ws.send(JSON.stringify(errMessage));
+        } else {
+          const successMessage = {
+            type: "success",
+            payload: {
+              data: "Folder created successfully",
+            },
+          };
+          ws.send(JSON.stringify(successMessage));
+        }
+      });
+      break;
+    case "deleteFolder":
+      fs.rmdir(pathToFileOrFolder, { recursive: true }, (err) => {
+        if (err) {
+          console.log(err);
+          const errMessage = {
+            type: "error",
+            payload: {
+              data: "Could not delete folder",
+            },
+          };
+          ws.send(JSON.stringify(errMessage));
+        } else {
+          const successMessage = {
+            type: "success",
+            payload: {
+              data: "Folder deleted successfully",
+            },
+          };
+          ws.send(JSON.stringify(successMessage));
+        }
+      });
+      break;
+    default:
+      console.log("Invalid type");
+      const errMessage = {
+        type: "error",
+        payload: {
+          data: "Invalid type",
+        },
+      };
+      ws.send(JSON.stringify(errMessage));
+      break;
+  }
 };
 
 module.exports = handleWebSocketEvents;
